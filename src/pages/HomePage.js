6f0b926b2d6eebd6a15fdf3375d2e150d2feb3f9
Jack <jlauravelez@gmail.com>
2019-05-30 07:58:43 +0200

    adding link
    button search

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
+import {Link, BrowserRouter} from "react-router-dom";
 import { Layout } from 'antd';
+
 import 'antd/dist/antd.css';
+import wroclaw from '../pages/page.css'
 
 const { Content } = Layout;
 
 class HomePage extends Component {
+
 
     constructor() {
         super();
         this.state = {
             search: 'Enter City'
         }
+    }
+
+    onNavigateProfiles(){
+        this.props.history.push("/ProfileSearch");
+
+    }
+
+    onNavigateTours(){
+        this.props.history.push("/TourSearch");
+
     }
 
     updateSearch(event) {
@@ -20,13 +34,25 @@ class HomePage extends Component {
 
     render() {
 		return (
-            <div className="content">
-				    <h1 style={{ color: 'white' }}>Discover places all around the world</h1>
-                    <h3 style={{ color: 'white' }}>With the best tour guide of the city</h3>
+            
+            <div >
+                <header>
+				    <h1 style={{ color: 'black' }}>Discover places all around the world</h1>
+                    <h3 style={{ color: 'black' }}>With the best tour guide of the city</h3>
+                    </header>
                     <div className="HomeBox">
+
                     <div class="ui input"><input type="text" placeholder="Search..." /></div>
+                    
                     </div>
+                    <div>
+                    <button onClick={this.onNavigateProfiles.bind(this)} classname = "btn btn-primary">Search by Profile</button>
+                    <button onClick={this.onNavigateTours.bind(this)} classname = "btn btn-primary">Search by Tour</button>
+                    
+                    </div>
+                    
             </div>
+            
         );
     }
 }
---
+++ src/pages/ProfileSearch.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Label, Menu, Tab, Icon, Button, Loader } from 'semantic-ui-react'
+import { Layout } from 'antd';
+import 'antd/dist/antd.css';
+
+class ProfileSearch extends Component {
+    render() {
+        return (<div>Tourist Page</div>
+
+        );
+    }
+}
+
+export default ProfileSearch;

00a78e420b6fe0bad1d07e34d2d5c3b44305103f
Fuat Kara <fuatkara.95@gmail.com>
2019-05-30 11:16:57 +0200

    Jackie
    Hi I have done the Aboutpage I tried to add some component in Homepage,TourGide and User Page.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -15,6 +15,9 @@ import tour2 from '../images/tour2.PNG';
 import tour3 from '../images/tour3.PNG';
 import pp1 from '../images/pp1.jpg';
 import pp2 from '../images/pp2.jpg';
+import { Label, Menu, Tab, Icon, Button, Loader } from 'semantic-ui-react';
+import logo from '../images/logo.png';
+
 const { Content } = Layout;
 
 class HomePage extends Component {
@@ -88,19 +91,35 @@ class HomePage extends Component {
                     </header>
                     <img class ="resize2" src={pp1} alt="pp1" />
                     <img class ="resize2" src={pp2} alt="pp2" />
-                   
-
-              
-            </Modal>
+              </Modal>
+                <br />
+                <h1 class="lineDikey"
+                    style={{ fontSize:'18px',color: '#696969'}} 
+                    className="inlineS">
+                    Contact With Us
+                    <ul>
+                        <li>EXETER, ON (Head Office)</li>
+                        <li> 311 Main St., PO Box 1990, N0M 1S7 </li>
+                        <li>519-235-2000</li>
+                        <h1 style={{ color: '#696969'}} className = "spaceTop">
+                            <img style={{fontSize:'2'}} src={logo} alt="Logo"/>
+                            Traveling with ease
+                        </h1> 
+                    </ul>
+                </h1>
+                <br />
+                <p 
+                    style={{backgroundColor: 'black',fontSize:20 ,padding: 1}} 
+                    className="footer"
+                >
+                <Menu.Item >
+                  <Link to="/HomePage" ><Icon type="user" />Home Page | </Link>
+                  <Link to="/UserPage" ><Icon type="user" /> User Page | </Link>
+                  <Link to="/AboutPage" ><Icon type="user" /> About Page | </Link>
+                  <Link to="/TourGuidePage" ><Icon type="user" /> Profile Page | </Link>
+                </Menu.Item>
+                </p>
           </div>
-                
-                   
-
-                   
-                        
-                        
-      
-           
         );
     }
 }
--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
 import {
-    Form, Input, Tooltip, Icon, Cascader, Select, Row, Col, Checkbox, Button, AutoComplete,
+    Form, Input, Tooltip, Cascader, Select, Row, Col, Checkbox, AutoComplete,
   } from 'antd';
 import 'antd/dist/antd.css';
 import firebase_app from "../base";
 import TV from "../components/Trip_View";
 import TourGuidePage from './TourGuidePage';
 import TouristPage from './TouristPage';
+import {
+  Link
+} from "react-router-dom";
+import logo from '../images/logo.png';
+import { Label, Menu, Tab, Icon, Button, Loader } from 'semantic-ui-react';
 
 const { Option } = Select;
 const AutoCompleteOption = AutoComplete.Option;
@@ -14,11 +19,11 @@ const AutoCompleteOption = AutoComplete.Option;
 class UserPage extends Component {
     render() {
       return (
-
         localStorage.getItem('user-type') === true || localStorage.getItem('user-type') === 'tour-guide' ?
         <TourGuidePage />
         :
         <TV />
+
       );
     }
   }
